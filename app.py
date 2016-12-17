c31b5f8c5a249f34b50b87c327ceb612afb5e697
gabisia <gabisia@users.noreply.github.com>
2016-12-17 09:34:03 -0800

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,10 @@ def processRequest(req):
     if yql_query is None:
         return {}
     yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
+    
+    print("YQL:")
+    print(yql_url)
+    
     result = urllib.urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
